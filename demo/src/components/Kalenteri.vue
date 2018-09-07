2ae2dbec388f1f498052227d4411929da295f184
Jani Oksanen <oksanen.jani@outlook.com>
2018-09-07 11:28:24 +0300

    Vähän viestiä käytälle, mikäli antaa väpäivämäärän väärässä muodossa

--- demo/src/components/Kalenteri.vue
+++ demo/src/components/Kalenteri.vue
@@ -12,8 +12,9 @@
 <template class="test">
   <div>
     <p class="center-text">Valittu päiväys: <strong>{{ pvm }}</strong></p>
+    <p v-if="showError">Väärä formaatti. Anna muodossa 12.12.2012</p>
     <v-menu ref="menu1" :close-on-content-click="false" v-model="menu1" :nudge-right="40" lazy offset-y transition="scale-transition" full-width>
-      <v-text-field slot="activator" v-model="calendarDate" label="Klikkaa antaaksesi päivämäärä" persistent-hint prepend-icon="event" @change="parseDate"></v-text-field>
+      <v-text-field v-bind:style="{border: bordered }" slot="activator" v-model="calendarDate" label="Klikkaa antaaksesi päivämäärä" persistent-hint prepend-icon="event" @change="parseDate"></v-text-field>
       <v-date-picker v-model="calendarModelDate" first-day-of-week=1 locale="fi-fi" no-title @input="reformatDate"></v-date-picker>
     </v-menu>
   </div>
@@ -31,9 +32,16 @@ export default {
       pvm: moment(new Date()).format('DD.MM.YYYY').toString(),
       menu1: false,
       calendarDate: moment(new Date()).format('DD.MM.YYYY').toString(),
-      calendarModelDate: null
+      calendarModelDate: null,
+      border: null,
+      showError: false
     }
 
+  },
+  computed: {
+    bordered() {
+      return this.border;
+    }
   },
   created() {
 
@@ -51,24 +59,16 @@ export default {
         const regex = /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/gm;
         if (regex.exec(input)) {
           this.pvm = input;
-
-
+          this.border = "";
+          this.showError = false;
+        } else {
+          this.showError = true;
+          this.border = " 2px solid red";
         }
       }
       this.calendarDate = this.pvm;
       this.calendarModelDate = this.parseCalendar(this.calendarDate);
       return this.pvm;
-
-
-
-      /* const regex = /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/gm;
-       if (regex.exec(input)) {
-         this.calendarDate = input;
-         this.pvm = this.calendarDate;
-         this.calendarModelDate = this.parseCalendar(this.calendarDate);
-
-       }
-       return this.pvm;*/
 
     },
     parseCalendar(paivamaara) {
@@ -77,7 +77,8 @@ export default {
       return parsed;
     },
     reformatDate(input) {
-
+      this.showError = false;
+      this.border = "";
       if (input == null) {
         return null;
       }
